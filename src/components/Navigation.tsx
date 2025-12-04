93f1dc6b4b208f138d84930ca822912b47a6a416
azisstywan <azissetiawan0813@gmail.com>
2025-12-04 23:12:10 +0700

    ganti logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import logoImg from "@/assets/logoo.png";
+import logoImg from "@/assets/logo katar.png";
 import ThemeToggle from "@/components/theme-toggle";
 
 const navItems = [
