840f70852c1aa555943197ecaf167b04f737c12c
azisstywan <azissetiawan0813@gmail.com>
2025-12-05 12:46:11 +0700

    tambah @

--- src/pages/Galeri.tsx
+++ src/pages/Galeri.tsx
@@ -30,7 +30,7 @@ const Galeri = () => {
       title: "Donor Darah Rutin",
       description: "Kegiatan donor darah bersama PMI",
       date: "Oktober 2024",
-      images: ["src/assets/hero-home.jpg", "src/assets/back-home.png"],
+      images: ["@/src/assets/hero-home.jpg", "@/src/assets/back-home.png"],
     },
     {
       id: 2,
